a0a1084f83aa52e79efb9e404679679a0b3962b8
sunlins <song.lin.86@gmail.com>
2022-02-13 21:03:58 +0800

    echo use bufio.NewReader

--- echo-server/main.go
+++ echo-server/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"io"
+	"bufio"
 	"log"
 	"net"
 )
@@ -12,39 +12,26 @@ import (
 func echo(conn net.Conn) {
 	defer conn.Close()
 
-	//创建一个缓冲区来存储接收到的数据
-	b := make([]byte, 512)
-
-	for {
-
-		//通过conn.Read接收数据到一个缓冲区
-		size, err := conn.Read(b[0:])
-
-		//接收到关闭信号
-		if err == io.EOF {
-			log.Println("Client disconnected")
-			break
-		}
-
-		if err != nil {
-			log.Println("Unexpected error")
-			break
-		}
-
-		log.Printf("Received %d bytes:%s\n", size, string(b))
-
-		//通过conn.Write发送数据
-		log.Println("Writing data")
-		if _, err := conn.Write(b[0:size]); err != nil {
-			log.Fatalln("Unable to write data")
-		}
+	reader :=bufio.NewReader(conn)
+	s,err :=reader.ReadString('\n')
+	if err!=nil{
+		log.Fatalln("Unable to read data")
 	}
+	log.Printf("Read %d bytes: %s",len(s),s)
+
+	log.Println("Writing data")
+	writer :=bufio.NewWriter(conn)
+	if _,err :=writer.WriteString(s);err!=nil{
+		log.Fatalln("Unable to write data")
+	}
+
+	writer.Flush()
 }
 
 func main() {
 
 	//绑定TCP端口20080
-	lister, err := net.Listen("tcp", ":20080")
+	listener, err := net.Listen("tcp", ":20080")
 	if err != nil {
 		log.Fatalln("Unable to bind to port")
 	}
@@ -52,7 +39,7 @@ func main() {
 
 	for {
 		//等待连接，在已建立的连接上创建net.conn
-		conn, err := lister.Accept()
+		conn, err := listener.Accept()
 		log.Println("Received connection")
 		if err != nil {
 			log.Fatalln("Unable to accept connection")
